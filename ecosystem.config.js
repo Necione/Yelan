0b51d1122728325cb8c7c1910fd82109cb0b2531
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2024-07-07 22:28:44 -0700

    don't set to production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,6 @@ module.exports = {
         name: "Yelan",
         script: "npm",
         args: "start",
-        env: {
-          NODE_ENV: "production",
-        },
         log_date_format: "YYYY-MM-DD HH:mm Z",
         min_uptime: "5m", // 5 minute
         max_restarts: 20,
