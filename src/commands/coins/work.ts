3d5fcb219f1b21a41a4c52053a59ef6736e99cc3
Necione <pqsttime@gmail.com>
2024-08-03 15:54:01 +0800

    dep

--- src/commands/coins/work.ts
+++ src/commands/coins/work.ts
@@ -1,11 +1,9 @@
 import type { SlashCommand } from "@elara-services/botbuilder";
-import { embedComment, formatNumber, get } from "@elara-services/utils";
 import { texts } from "@liyueharbor/econ";
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { channels } from "../../config";
-import { addBalance, getProfileByUserId } from "../../services";
-import { checks, cooldowns, locked, userLockedData } from "../../utils";
-import { workMessages } from "../../utils/work";
+import { getProfileByUserId } from "../../services";
+import { locked, userLockedData } from "../../utils";
 
 export const work: SlashCommand = {
     disabled: {
@@ -35,44 +33,16 @@ export const work: SlashCommand = {
             return responder.edit(userLockedData(interaction.user.id));
         }
 
-        const amount = Math.floor(Math.random() * 20) + 2;
-        const getMsg = () =>
-            workMessages[Math.floor(Math.random() * workMessages.length)];
-        const { message, image, title, footer } = getMsg();
-        const msg = message.replace(/%AMOUNT%/gi, formatNumber(amount));
-
-        if (checks.limit(data, amount)) {
-            locked.del(interaction.user.id);
-            return responder.edit(
-                embedComment(`You've reached your daily coin earning limit.`),
-            );
-        }
-        const hasCooldown = cooldowns.get(data, "work");
-        if (!hasCooldown.status) {
-            locked.del(interaction.user.id);
-            return responder.edit(embedComment(hasCooldown.message));
-        }
-
         locked.del(interaction.user.id);
         await Promise.all([
             responder.edit({
                 embeds: [
                     new EmbedBuilder()
                         .setColor("Aqua")
-                        .setTitle(title)
-                        .setDescription(msg)
-                        .setThumbnail(image)
-                        .setFooter({ text: footer }),
+                        .setTitle("`/work` is being depreciated")
+                        .setDescription("sWe're working on a new system to replace this command, sorry!")
                 ],
             }),
-            cooldowns.set(data, "work", get.hrs(1)),
-            addBalance(
-                interaction.user.id,
-                amount,
-                true,
-                `Via ${work.command.name}`,
-            ),
-            checks.set(data, amount),
         ]);
     },
 };
