df8c0b0849b3c8ae8f7919d65965f36b2bd0605a
Necione <pqsttime@gmail.com>
2024-09-10 21:18:31 -0700

    abyss

---
+++ src/utils/abyss/abyssFloor8.ts
@@ -0,0 +1,35 @@
+import { type AbyssMonster } from "../hunt";
+
+const abyssFloor8Monsters: AbyssMonster[] = [
+    {
+        name: "Treasure Hoarder Gravedigger",
+        minHp: 300,
+        maxHp: 400,
+        minDamage: 30,
+        maxDamage: 50,
+        image: "https://lh.elara.workers.dev/rpg/monsters/treasure_hoarder_gravedigger.png",
+        critChance: 25,
+        critValue: 1.5,
+        defChance: 50,
+        defValue: 50,
+        quantity: 2,
+    },
+    {
+        name: "Mutant Electro Slime",
+        minHp: 350,
+        maxHp: 400,
+        minDamage: 10,
+        maxDamage: 35,
+        image: "https://lh.elara.workers.dev/rpg/monsters/mutant_electro_slime.png",
+        critChance: 75,
+        critValue: 3,
+        defChance: 25,
+        defValue: 15,
+        quantity: 2,
+    },
+];
+
+const abyssFloor8Drops = [{ item: "Wanderer's Advice", amount: 2 }];
+
+export { abyssFloor8Drops, abyssFloor8Monsters };
+
---
+++ src/utils/abyss/abyssFloor9.ts
@@ -0,0 +1,35 @@
+import { type AbyssMonster } from "../hunt";
+
+const abyssFloor9Monsters: AbyssMonster[] = [
+    {
+        name: "Large Cryo Slime",
+        minHp: 100,
+        maxHp: 200,
+        minDamage: 25,
+        maxDamage: 50,
+        image: "https://lh.elara.workers.dev/rpg/monsters/large_cryo_slime.png",
+        critChance: 50,
+        critValue: 1.5,
+        defChance: 50,
+        defValue: 50,
+        quantity: 5,
+    },
+    {
+        name: "Hydro Hilichurl Rogue",
+        minHp: 500,
+        maxHp: 600,
+        minDamage: 30,
+        maxDamage: 75,
+        image: "https://lh.elara.workers.dev/rpg/monsters/hydro_hilichurl_rogue.png",
+        critChance: 25,
+        critValue: 1.5,
+        defChance: 50,
+        defValue: 50,
+        quantity: 1,
+    },
+];
+
+const abyssFloor9Drops = [{ item: "Wanderer's Advice", amount: 2 }];
+
+export { abyssFloor9Drops, abyssFloor9Monsters };
+
