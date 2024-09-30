6dff54d9f7669cec231cf69a2e87ba8e8e725276
Necione <pqsttime@gmail.com>
2024-09-29 22:23:58 -0700

    Revert "Update adventure.ts"
    This reverts commit 87f44a018b2e9990bfddca2ccd434b4441d10416.

--- src/commands/rpg/adventure.ts
+++ src/commands/rpg/adventure.ts
@@ -114,10 +114,6 @@ export const adventure = buildCommand<SlashCommand>({
                     "No stats found for you, please set up your profile.",
                 );
             }
-            
-            if (stats.location === "Liyue Harbor") {
-                throw new Error("You cannot go on an adventure in Liyue Harbor!");
-            }            
 
             if (stats.isTravelling) {
                 throw new Error(
