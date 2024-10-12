a7cb970b4265869c93edb0ea9cfd3f0ce2f57ba3
i-bz <isamemeamongus@duck.com>
2024-10-12 22:19:36 +0530

    give payroll payment perms in both servers

--- src/commands/staff/payment.ts
+++ src/commands/staff/payment.ts
@@ -8,7 +8,7 @@ import { logs } from "../../utils";
 
 export const payment = buildCommand<SlashCommand>({
     locked: {
-        roles: [...roles.main, roles.management.econ, roles.payroll],
+        roles: [...roles.main, ...roles.management.econ],
     },
     command: new SlashCommandBuilder()
         .setName(`payment`)
