2dfb260b4bc53909ac77ff146b9061329ee60d74
i-bz <isamemeamongus@duck.com>
2024-10-12 22:28:16 +0530

    Revert "give payroll payment perms in both servers"
    This reverts commit a7cb970b4265869c93edb0ea9cfd3f0ce2f57ba3.

--- src/commands/staff/payment.ts
+++ src/commands/staff/payment.ts
@@ -8,7 +8,7 @@ import { logs } from "../../utils";
 
 export const payment = buildCommand<SlashCommand>({
     locked: {
-        roles: [...roles.main, ...roles.management.econ],
+        roles: [...roles.main, roles.management.econ, roles.payroll],
     },
     command: new SlashCommandBuilder()
         .setName(`payment`)
